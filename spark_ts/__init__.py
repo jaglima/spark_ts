29e3b7663309d7d6dd3f625598526670395d64a0
jaglima <jaglima1984@gmail.com>
2018-07-25 16:15:44 -0300

    Code inclusion

--- spark_ts/__init__.py
+++ spark_ts/__init__.py
@@ -1,13 +1,33 @@
+# This code is part of the Spark_ts distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
+"""Collection of modules for dealing with time series data in Pyspark.
+"""
+import warnings
 from pyspark.sql.dataframe import DataFrame
 from .spark_ts import *
 
 
- 
+__version__ = "0.1.0"
+
+
+class spark_ts_warning(Warning):
+    def __init__(self):
+        warnings.warn(
+	    """spark_ts warning.
+	    	Spark_ts is experimental code at the current level of development.
+	    	"""
+	)    
+    pass
 
 
 
-# Patching the DataFrame object to implements transform functionality
+""" 
+Patching the DataFrame object adding transform functionality to Pyspark 
+Dataframe
+"""
 def transform(self, f):
+
     return f(self)
 
 DataFrame.transform = transform
--- spark_ts/spark_ts.py
+++ spark_ts/spark_ts.py
@@ -4,9 +4,11 @@ from pyspark.sql import functions as fun
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.window import Window
 
-# Summary compute missing data statistics for the dataframe
-# INPUT: Receive a dataframe containing tabular data
-# OUTPUT: Returns a dictionary with key column names and as values the percentual of missing for the columns
+""" 
+Summary: compute missing data statistics for the dataframe
+INPUT: Receive a dataframe containing tabular data
+OUTPUT: Returns a dictionary with key column names and as values the percentual of missing for the columns
+"""
 
 def summary(df):
     for k, v in sorted(
@@ -24,31 +26,54 @@ def summary(df):
         ):
         print (k, v)
 
-def findGap(df, date):
-	from pyspark.sql.functions import datediff, to_date, lit
+
+""" 
+Summary: Given a Dataframe object with a date column, search for max and minimum date in the range
+INPUT: Receive a Dataframe and the name of a datetype column
+OUTPUT: Returns the max and minimal dates in the range
+""" 
+def dateLimits(colName):
+    def inner(df):
+        from pyspark.sql import Row
+        from pyspark.sql.functions import from_unixtime, unix_timestamp, min, max
+
+        df.withColumn(colName,  unix_timestamp(df[colName])).agg(
+            from_unixtime(min(colName)).alias("min_ts"),
+            from_unixtime(max(colName)).alias("max_ts")
+        ).show()
+
+    return inner
+
+""" 
+Summary: Given a Dataframe object with a date column, returns gaps between two consecutive dates for 
+	each interval greater then limit
+INPUT: Receive a Dataframe, the name of a datetype column and a integer limit
+OUTPUT: Returns all the data ranges greater than limit
+""" 
+def gaps(colName):
+    def inner(df):
+        from pyspark.sql.functions import lag, col, datediff
+
+        w = Window().partitionBy().orderBy(col(colName))
+        df1 = (df
+               .select(colName, lag(colName)
+               .over(w)
+               .alias("lagged_col"))
+               .na
+               .drop()
+              )
+        (df1
+             .withColumn('diff_col', datediff(df1[colName], df1['lagged_col']))
+            .sort('diff_col', ascending=False)
+            .show()
+        )
+        return df
+    return inner
+
 
 """
-    for k, v in sorted(
-        df
-        .agg(*[
-            fun.round((1 - fun.count(c) / fun.count('*')),2)
-            .alias(c + '_miss')
-            for c in df.columns
-        ])
-        .collect()[0]
-        .asDict()
-        .items()
-        , key=lambda el: el[1]
-        ,reverse = True
-        ):
-        print (k, v)"""
-
-
-	df.withColumn(date, 
-              datediff(to_date(lit("2017-05-02")),
-                       to_date("low","yyyy/MM/dd"))).show()
-
-# Interpolate missing data from a given column
+Interpolate missing data from a given column
+"""
 def interp(col, precision):
     def inner(df):
         window = Window.partitionBy("Estacao").orderBy("Data").rowsBetween(-2, 2)
@@ -68,7 +93,7 @@ def interp(col, precision):
 def outliars(col):
     def inner(df):
         # compute monthly average 
-        window = Window.partitionBy("Estacao").orderBy("Data").rowsBetween(-5, 0)
+        window = Window.partitionBy("Estacao").orderBy("Data").rowsBetween(-30, 0)
         df = df.withColumn("movingAvg", fun.avg(df[col]).over(window))
 
         # compute monthly stdev
@@ -82,4 +107,5 @@ def outliars(col):
     return inner
 
 
-__all__ = ['outliars', 'interp', 'summary']
+
+__all__ = ['outliars', 'interp', 'summary', 'dateLimits', 'gaps']
